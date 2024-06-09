2187d03b8863affd8923814b6135fff9d7183d6f
Wassim Metallaoui <wassim@metallaoui.com>
2024-06-08 20:34:52 -0500

    feat: support instal macs too

--- src/lib/cli.ts
+++ src/lib/cli.ts
@@ -1,6 +1,7 @@
 import { environment } from "@raycast/api";
 import path from "path/posix";
 import fs from "fs";
+import os from "os";
 import afs from "fs/promises";
 import download from "download";
 import * as tar from "tar";
@@ -8,11 +9,19 @@ import { exec } from "child_process";
 import sha256 from "sha256-file";
 
 const cliVersion = "0.2.2";
-const cliFileInfo = {
-  arch: "aarch64",
-  pkg: "elgato-light-Darwin-aarch64.tar.gz",
-  sha256: "20899f0ede797c6d13893b792b705454b896085f4188bc816f655a97bc2c09e1",
+const cliFileInfos = {
+  x64: {
+    arch: "x86_64",
+    pkg: "elgato-light-Darwin-x86_64.tar.gz",
+    sha256: "60ee7c0bfccca79cd504c27336a4aaf5ef492cb94e0d2a6d5a46f2fc1258889a",
+  },
+  arm64: {
+    arch: "aarch64",
+    pkg: "elgato-light-Darwin-aarch64.tar.gz",
+    sha256: "20899f0ede797c6d13893b792b705454b896085f4188bc816f655a97bc2c09e1",
+  },
 };
+const cliFileInfo = os.arch() === "arm64" ? cliFileInfos.arm64 : cliFileInfos.x64;
 
 async function sha256FileHash(filename: string): Promise<string | null> {
   return new Promise((resolve, reject) => {
